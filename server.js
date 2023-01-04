845a1823440f29bd15d39f049462e15cf011f937
Gowtham-Chokkalingam <gowthamtceian@gmail.com>
2023-01-04 23:36:30 +0530

    added mock server

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const jsonServer = require("json-server"); // importing json-server library
+const jsonServer = require("json-server"); 
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const PORT = 8080; 
+const PORT = process.env.PORT||8080; 
 
 server.use(middlewares);
 server.use(router);
